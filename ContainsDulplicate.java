98b526ebac5db3d9a2b7fc5d372768e7c8b97b64
WilbertFV <villalow@kean.edu>
2023-08-12 00:17:13 -0400

    Test

---
+++ ContainsDulplicate.java
@@ -0,0 +1,18 @@
+// page for question: https://leetcode.com/problems/contains-duplicate/
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+  public boolean containsDuplicate(int[] nums) {
+      Set<Integer> uniques = new HashSet<>();
+      for (int i = 0; i < nums.length; i++) {
+          if (uniques.contains(nums[i])) {
+              return true;
+          }
+          uniques.add(nums[i]);
+      }
+      return false;
+  }
+}
---
+++ ContainsDuplicate.py
@@ -0,0 +1,13 @@
+# question: https://leetcode.com/problems/contains-duplicate/
+
+from ast import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
