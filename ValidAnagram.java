f936d31c9908086cb6b7bb0ba408dacf13c47f82
WilbertFV <villalow@kean.edu>
2023-08-12 20:58:06 -0400

    valid anagram

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -1,7 +1,5 @@
 // https://leetcode.com/problems/valid-anagram/
-public static void main(String[] args) {
-  
-
+ 
 class Solution {
 
   public boolean isAnagram(String s, String t) {
@@ -18,5 +16,13 @@ class Solution {
 
       return true;
   }
+
+public static void main(String[] args) {
+
+    // You can call the method here if you want
+    Solution solution = new Solution();
+    String s = "anagram";
+    String t = "nagaram";
+    System.out.println("Is this an anagram?: "+ solution.isAnagram(s, t)); // Output will be true
 }
 }
---
+++ ValidAnagram.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/valid-anagram/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+      
+# Test the method
+solution = Solution()
+s = "anagram"
+t = "nagaram"
+result = solution.isAnagram(s, t)
+print("Is anagram:", result)  # Output will be True
